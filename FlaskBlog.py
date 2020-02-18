b74acbda8695d03903d7cd9d51e0b380ca3389c2
Covax <57707580+covax84@users.noreply.github.com>
2020-02-19 00:07:59 +0300

    updated main FlaskBlog.py and requirements.txt added pseudo-login for admin@blog.com with success flash message, given methods GET/POST to login

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -47,9 +47,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You Have Been Logged In', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Failed, check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 

1cffd669559f1be32f520b4a018e04910d5aff63
Covax84 <python.momentum@gmail.com>
2020-02-20 23:57:48 +0300

    SQLAlchemy added, +class User

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -6,10 +6,26 @@
 # python flaskblog.by  - same as flask run
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4f42ae8ecfa6c037c633728707169dac'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, promary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 posts = [
     {
